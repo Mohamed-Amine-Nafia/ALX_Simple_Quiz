61842d1886cdf91ae3935379bb5556792d1301f1
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-08-03 12:00:20 +0200

    fix the code

--- quiz.js
+++ quiz.js
@@ -1,24 +1,20 @@
-const answersContainer = document.getElementById("answers");
-
-const answers = answersContainer.querySelectorAll("input");
-
-const submitAnswer = document.getElementById("submit-answer");
-
-const feedback = document.getElementById("feedback");
-
 function checkAnswer() {
   const correctAnswer = "4";
-  let userAnswer;
-  answers.forEach((ele) => {
-    if (ele.checked) {
-      userAnswer = ele.value;
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+  const feedback = document.getElementById("feedback");
+
+  if (selectedOption) {
+    const userAnswer = selectedOption.value;
+
+    if (userAnswer === correctAnswer) {
+      feedback.textContent = "Correct! Well done.";
+    } else {
+      feedback.textContent = "That's incorrect. Try again!";
     }
-  });
-  if (userAnswer === correctAnswer) {
-    feedback.innerHTML = "Correct! Well done.";
   } else {
-    feedback.innerHTML = "That's incorrect. Try again!";
+    feedback.textContent = "Please select an answer before submitting.";
   }
 }
 
-submitAnswer.addEventListener("click", checkAnswer);
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
